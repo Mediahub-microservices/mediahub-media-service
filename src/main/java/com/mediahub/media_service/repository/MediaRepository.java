f02fe6ee782da3d1473ed6ed7f84810016302799
fatima-ezzahraalouane <falouane38@gmail.com>
2026-03-10 23:22:35 +0000

    L'implémentation de MediaRepository

---
+++ src/main/java/com/mediahub/media_service/repository/MediaRepository.java
@@ -0,0 +1,21 @@
+package com.mediahub.media_service.repository;
+
+import com.mediahub.media_service.domain.entity.Media;
+import com.mediahub.media_service.domain.enums.Category;
+import com.mediahub.media_service.domain.enums.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MediaRepository extends JpaRepository<Media, Long> {
+
+    List<Media> findByGenre(Genre genre);
+
+    List<Media> findByTitleContainingIgnoreCase(String title);
+
+    List<Media> findByTitleContainingIgnoreCaseAndGenre(String title, Genre genre);
+
+    List<Media> findByCategory(Category category);
+
+    List<Media> findByTitleContainingIgnoreCaseAndCategory(String title, Category category);
+}
